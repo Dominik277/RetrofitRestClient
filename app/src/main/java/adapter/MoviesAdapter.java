dc9ac3987e974caec7d08b60647f3f5235d5a776
Dominik277 <dominikdoric277@gmail.com>
2020-12-22 22:33:45 +0100

    Dodane sve metode u MoviesAdapter klasu te također unutar ove klase dodana jos jedna metoda ViewHolder.

--- app/src/main/java/adapter/MoviesAdapter.java
+++ app/src/main/java/adapter/MoviesAdapter.java
@@ -1,4 +1,35 @@
 package adapter;
 
-public class MoviesAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MoviesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
