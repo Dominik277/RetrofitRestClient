a56a87c3c2e569bd73d12431bc7a22464af1f689
Dominik277 <dominikdoric277@gmail.com>
2020-12-22 22:44:53 +0100

    Dovrsena i klasa MoviesAdapter.

--- app/src/main/java/adapter/MoviesAdapter.java
+++ app/src/main/java/adapter/MoviesAdapter.java
@@ -1,34 +1,64 @@
 package adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import dagger2.and.retrofit.R;
+import model.Movie;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
 
+    private List<Movie> movies;
+    private Context context;
+
+    public MoviesAdapter(Context context,List<Movie> movies) {
+        this.movies = movies;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public MoviesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie,parent,false);
+
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MoviesAdapter.ViewHolder holder, int position) {
 
+        Movie movie = movies.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.director.setText(movie.getDirector());
+        holder.description.setText(movie.getDescription());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        private TextView title;
+        private TextView director;
+        private TextView description;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            title = itemView.findViewById(R.id.title);
+            director = itemView.findViewById(R.id.director);
+            description = itemView.findViewById(R.id.description);
         }
     }
 
